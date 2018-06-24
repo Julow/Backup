83434b5568717074f30d7b2a8dda918a4566179c
juloo <juloo.dsi@gmail.com>
2018-06-24 08:43:23 +0200

    Move the configs to the top and put everything in a tar archive

--- sync.sh
+++ sync.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
-
-# Go to the script's directory
 cd ${0%/*}
-PATH="$(pwd):$PATH"
+
+# -
+# Configs
 
 # The folder that contains backup datas
 BACKUP_LOCATION=$(pwd)/backup
 
-mkdir -p "$BACKUP_LOCATION"
-cd "$BACKUP_LOCATION"
+# Commands to run
+# They are run in the $BACKUP_LOCATION directory
+# sync_* scripts are available
+sync ()
+{
+	sync_github.sh "Julow"
+	sync_user.sh ~/.presets ~/Documents/config
+}
+
+# End of configs
+# -
+
+name_noclash ()
+{
+	NAME="$1$2"
+	i=1
+	while [[ -e "$NAME" ]]
+	do
+		NAME="$1-$i$2"
+		(( i++ ))
+	done
+	echo "$NAME"
+}
 
 # Sync
-sync_github.sh "Julow"
-sync_user.sh ~/.presets ~/Documents/config
+PATH="$(pwd):$PATH"
+mkdir -p "$BACKUP_LOCATION"
+( cd "$BACKUP_LOCATION"; sync )
+
+# Build the archive
+BACKUP_NAME=$(name_noclash "backup-$(date +"%F")" ".tar.gz")
+tar -czf "$BACKUP_NAME" -C "$BACKUP_LOCATION" .
