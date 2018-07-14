bf3334b61512d6d746ef4618ddee83484ac78ea7
juloo <juloo.dsi@gmail.com>
2018-07-14 05:47:48 +0200

    sync_git.sh: Backup staged, unstaged and untracked changes

--- sync_git.sh
+++ sync_git.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
-set -e
 
 # Backup git repositories
+# For local repositories, backups staged, unstaged and untracked files
+
+untracked_changes ()
+{
+	if [[ -e .gitignore ]]
+	then IGNORE="-X .gitignore"
+	else IGNORE=""
+	fi
+	for f in $(git ls-files -o $IGNORE)
+	do
+		if [[ -d $f ]]
+		then echo "Warning: $f: Untracked change cannot be backup" >&2
+		else git diff --binary /dev/null "$f"
+		fi
+	done
+}
+
+update_patches ()
+{
+	if [[ -d "$1" ]]
+	then
+		DST=$(pwd)
+		cd "$1"
+		git diff --staged > "$DST/staged-changes.patch"
+		git diff > "$DST/unstaged-changes.patch"
+		untracked_changes > "$DST/untracked-changes.patch"
+	fi
+}
 
 for uri in "$@"
 do
@@ -10,9 +37,10 @@ do
 	echo "$uri -> $DST"
 	if [[ -d $DST ]]; then
 		# Already cloned, just update
-		( cd "$DST"; git remote update )
+		( cd "$DST"; git remote update; update_patches "$uri" )
 	else
 		mkdir -p "$DST"
 		git clone --mirror -q --progress -- "$uri" "$DST"
+		( cd "$DST"; update_patches "$uri" )
 	fi
 done
